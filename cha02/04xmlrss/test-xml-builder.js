695afb069f8f34f383693488f3afffd8b745f09a
TK_Ratina <taka4.kato@gmail.com>
2016-03-25 08:52:34 +0900

    xmlに整形

---
+++ cha02/04xmlrss/test-xml-builder.js
@@ -0,0 +1,12 @@
+//モジュールの取り込み
+var xml2js = require('xml2js');
+
+//変換元オブジェクト
+var obj = {
+  item:{name:"Banana", price:150}
+};
+
+//xmlに変換
+var builder = new xml2js.Builder();
+var xml = builder.buildObject(obj);
+console.log(xml);
