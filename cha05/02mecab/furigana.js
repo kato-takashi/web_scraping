c4784a446d1a14c04d2ff0f8b830c5408a99290a
TK_Ratina <taka4.kato@gmail.com>
2016-05-10 08:43:17 +0900

    P227形態素解析　振り仮名をふる

---
+++ cha05/02mecab/furigana.js
@@ -0,0 +1,44 @@
+//振り仮名を振る for node.js
+//モジュールの取り込み
+var fs = require('fs');
+var Mecab = require('./mecab-mod.js');
+var mecab = new Mecab();
+
+//コマンドラインを調べる
+var args = process.argv;
+args.shift(); //nodeを除去
+args.shift(); //スクリプト名の除去
+
+//引数がなければプログラムの使い方を表示する
+if(args.length <= 0){
+  console.log("[USAGE] furigana.js 入力テキスト");
+  process.exit();
+}
+
+//入力ファイルを読み込む
+var inputfile = args.shift();
+var txt = fs.readFileSync(inputfile, "utf-8");
+
+//形態素解析する
+mecab.parse(txt, function(err, items){
+  var res = "";
+  for(var i in  items){
+    var k = items[i];
+    var word = k[0];
+    var kana = k[8];
+    if(k == "EOS") continue;
+
+    //振り仮名が必要な時を判定
+    if(word == kana || isHiranaga(word)){
+      res += word;
+    }else {
+      res += word + '(' + kana + ')'
+    }
+  }
+  console.log(res);
+});
+
+//ひらがな判定
+function isHiranaga(s){
+  return (s.match(/^[あ-ん]+$/));
+}
